3776b9e709d1af13ab140c43189fb428112647e0
ferna <ferna@DESKTOP-GBDDPLS>
2022-11-09 14:12:59 -0300

    Add project files.

---
+++ cadastroTarefas-assino/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace cadastroTarefas_assino.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
