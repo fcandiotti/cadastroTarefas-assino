f3692431a0ef057e186772958dbb7746a492271b
Fernando Candiotti <fernando_candiotti@hotmail.com>
2022-11-09 16:21:51 -0300

    Cria Front-end da aplicação

---
+++ cadastroTarefas-assino/Controllers/TarefaController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace cadastroTarefas_assino.Controllers
+{
+    public class TarefaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Criar()
+        {
+            return View();
+        }
+        public IActionResult Editar()
+        {
+            return View();
+        }
+        public IActionResult DeletarConfirmacao()
+        {
+            return View();
+        }
+    }
+}
